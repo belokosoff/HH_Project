7588e44cf390e9359da868158d7ac96722f18aa5
Maxim Belokosov <max.belokosoff@gmail.com>
2025-08-17 09:38:16 +0300

    Initial Commit

---
+++ HH_HomeStore/HH_HomeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  HH_HomeStoreApp.swift
+//  HH_HomeStore
+//
+//  Created by Maxim Belokosov on 17.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct HH_HomeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
